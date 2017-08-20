082f964d92de38792ca24120df26a5c3693c763b
railsmaxim <maxximver@outlook.com>
2017-08-20 20:06:34 +0300

    Generate Home Controller

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,5 @@
+class HomeController < ApplicationController
+  def index
+    @hello_guest = 'Helo Again'
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :posts
+  resources :home
   root 'posts#index'
 end
