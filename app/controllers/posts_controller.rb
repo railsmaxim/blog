c0f96abcf87604e06504041ca0057cc0582f84ca
railsmaxim <maxximver@outlook.com>
2017-08-23 21:55:10 +0300

    All Right Now

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,38 +1,17 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
-  # GET /posts
-  # GET /posts.json
   def index
     @posts = Post.all
   end
 
-  # GET /posts/1
-  # GET /posts/1.json
   def show
   end
 
-  # GET /posts/new
   def new
     @post = Post.new
   end
 
-  # GET /posts/1/edit
-  def edit
-  end
-
-  def update
-    if @post.update(post_params)
-      redirect_to @post,
-      notice: 'Post was updated.'
-    else
-      redirect_to @post,
-      notice: 'Post was not updated.'
-    end
-  end
-
-  # POST /posts
-  # POST /posts.json
   def create
     @post = Post.new(post_params)
 
