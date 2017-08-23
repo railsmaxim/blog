57094932e13bf76c641a3437dd9cb621359fab6f
railsmaxim <maxximver@outlook.com>
2017-08-23 22:36:50 +0300

    hello

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,24 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show]
+  before_action :set_post, only: [:show, :edit, :update]
 
   def index
     @posts = Post.all
   end
 
   def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to @post,
+      notice: 'Post was updated.'
+    else
+      render :edit,
+      notice: 'Post was not updated.'
+    end
   end
 
   private
