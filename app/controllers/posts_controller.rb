6a54e6160fe5ce4641979684120e4d7bf0a15433
railsmaxim <maxximver@outlook.com>
2017-08-23 22:30:58 +0300

    Start

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,36 +1,11 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :set_post, only: [:show]
 
   def index
     @posts = Post.all
   end
 
   def show
-  end
-
-  def new
-    @post = Post.new
-  end
-
-  def create
-    @post = Post.new(post_params)
-
-    respond_to do |format|
-      if @post.save
-        format.html { redirect_to @post, notice: 'Post was successfully created.' }
-        format.json { render :show, status: :created, location: @post }
-      else
-        format.html { render :new }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  def destroy
-    @post.destroy
-
-    redirect_to posts_path,
-    notice: 'Post was successfully deleted.'
   end
 
   private
