c9939c79f92a40eb075799cf9d5c9d34c618ab12
railsmaxim <maxximver@outlook.com>
2017-08-23 22:44:50 +0300

    done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update]
+  before_action :set_post, only: [:show]
 
   def index
     @posts = Post.all
