ef695a48986a91ff2c9c6539077d468f04792df9
railsmaxim <maxximver@outlook.com>
2017-08-25 22:23:34 +0300

    rails

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,49 +1,11 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :set_post, only: [:show]
 
   def index
     @posts = Post.all
   end
 
   def show
-  end
-
-  def new
-    @post = Post.new
-  end
-
-  def create
-    @post = Post.new(post_params)
-    if @post.save
-      redirect_to @post,
-      notice: 'Post was created.'
-    else
-      render :new,
-      notice: 'Post was not created'
-    end
-  end
-
-  def edit
-  end
-
-  def update
-    if @post.update(post_params)
-      redirect_to @post,
-      notice: 'Post was updated.'
-    else
-      render :edit,
-      notice: 'Post was not updated.'
-    end
-  end
-
-  def destroy
-    if @post.destroy
-      redirect_to root_path,
-      notice: 'Post was deleted.'
-    else
-      redirect_to root_path,
-      notice: 'Post was not deleted'
-    end
   end
 
   private
@@ -57,9 +19,6 @@ class PostsController < ApplicationController
       ).permit(
         :title,
         :description,
-        :image,
-        :imagetwo,
-        :imagethree
       )
     end
 end
